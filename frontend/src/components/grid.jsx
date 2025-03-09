787ba3eb839c0aec3066c4fc852f8c635991debb
Rishav <bagririshav01@gmail.com>
2025-03-10 01:29:39 +0530

    added snake movement and images

--- frontend/src/components/grid.jsx
+++ frontend/src/components/grid.jsx
@@ -3,10 +3,12 @@ import { Tile } from "./tile";
 
 export const Grid = ({ row, col }) => {
     const [player,setPlayer]=useState([15,7])
+    const [snake1,setSnake1]=useState([10,10])
+    const [snake2,setSnake2]=useState([7,4])
     return (
         <div className="grid" style={{ display: "grid", gridTemplateColumns: `repeat(${col}, 1fr)`, gap: "0px" }}>
             {Array.from({ length: row * col }).map((_, index) => (
-                <Tile player={player} setPlayer={setPlayer} index={index} />
+                <Tile player={player} setPlayer={setPlayer} snake1={snake1} setSnake1={setSnake1} snake2={snake2} setSnake2={setSnake2} index={index} />
             ))}
         </div>
     );
--- frontend/src/components/tile.jsx
+++ frontend/src/components/tile.jsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
+import { snakePos } from "../algorithms/snakeMovement";
 
-export const Tile=({index,player,setPlayer})=>{
+export const Tile=({index,player,setPlayer,snake1,snake2,setSnake1,setSnake2})=>{
     const obstacleMatrix = [
-        ['g', 't', 'o', 't', 't', 't', 't', 'o', 't', 't', 't', 't', 't', 'o', 't'],
+        ['t', 't', 'o', 't', 't', 't', 't', 'o', 't', 't', 't', 't', 't', 'o', 't'],
         ['t', 't', 't', 'o', 't', 't', 'o', 't', 't', 't', 'o', 't', 't', 't', 't'],
         ['o', 't', 't', 't', 't', 'o', 't', 't', 'o', 't', 't', 't', 't', 'o', 't'],
-        ['t', 'o', 't', 't', 'o', 't', 'g', 't', 't', 't', 'o', 't', 'o', 't', 't'],
+        ['t', 'o', 't', 't', 'o', 't', 't', 't', 't', 't', 'o', 't', 'o', 't', 't'],
         ['t', 't', 'o', 't', 't', 't', 'o', 't', 't', 't', 't', 't', 't', 't', 'o'],
         ['t', 'o', 't', 't', 't', 't', 't', 'o', 't', 'o', 't', 't', 't', 'o', 't'],
         ['t', 't', 't', 'o', 't', 't', 'o', 't', 't', 't', 'o', 't', 't', 't', 't'],
@@ -19,11 +20,8 @@ export const Tile=({index,player,setPlayer})=>{
         ['t', 't', 'o', 't', 't', 't', 'o', 't', 't', 't', 't', 't', 't', 't', 'o'],
         ['t', 'o', 't', 't', 't', 't', 't', 't', 't', 'o', 't', 't', 't', 'o', 't'],
     ];
-    // const [player,setPlayer]=useState([15,7])
     const fn=(index)=>{
-        if(Math.floor(index / 15)==player[0]&&index % 15==player[1])return "bg-blue-700"
-        if(obstacleMatrix[Math.floor(index / 15)][index % 15]=='o')return "bg-gray-700"
-        if(obstacleMatrix[Math.floor(index / 15)][index % 15]=='g')return "bg-green-400"
+        if(obstacleMatrix[Math.floor(index / 15)][index % 15]=='t')return "bg-gray-400"
         return "bg-yellow-800"
     }
     return <div onClick={()=>{
@@ -31,10 +29,37 @@ export const Tile=({index,player,setPlayer})=>{
         let c=index % 15
         if(obstacleMatrix[r][c]=='t'&&Math.abs(r-player[0])+Math.abs(c-player[1])==1){
             setPlayer([r,c])
+            setSnake1(snakePos(obstacleMatrix,snake1,player))
+            setSnake2(snakePos(obstacleMatrix,snake2,player))
+            
             console.log(player)
         }
-    }} key={index} className={`w-10 h-10 border rounded border-gray-700 ${fn(index)}`}>
-        {index}
+    }} key={index} className={`w-10 h-10 border rounded relative border-gray-700 ${fn(index)}`}>
+        {/* Snake 1 Image */}
+        {Math.floor(index / 15) == snake1[0] && index % 15 == snake1[1] && (
+            <img
+                src="/snake-removebg-preview.png"
+                className="absolute top-0 left-0 w-full h-full object-cover"
+                alt="Snake"
+            />
+        )}
+
+        {/* Snake 2 Image */}
+        {Math.floor(index / 15) == snake2[0] && index % 15 == snake2[1] && (
+            <img
+                src="/snake-removebg-preview.png"
+                className="absolute top-0 left-0 w-full h-full object-cover"
+                alt="Snake"
+            />
+        )}
+        {/* Snake 2 Image */}
+        {Math.floor(index / 15) == player[0] && index % 15 == player[1] && (
+            <img
+                src="player.png"
+                className="absolute top-0 left-0 w-full h-full object-cover"
+                alt="Snake"
+            />
+        )}
     </div>  
     
 }
